6931918fa35e68128bec614b22e45e9b18c8f26c
Kelly Zomer <git@kellyzomer.com>
2023-07-29 21:19:39 +0200

    first commit app, header, world map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './global.svg';
 import './App.css';
+import Map from './Map.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Travel with Kell</p>
       </header>
+      <p>Click on the highlighted country to read my travel blogposts</p>
+      <div className="Map-container"> {/* Add a container div */}
+        <Map /> {/* Render the Map component here */}
+      </div>
     </div>
   );
 }
